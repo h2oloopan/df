671c47edd564f2dacd056dd2ccc8e49c73236515
Shengying Pan <i@thepans.me>
2015-01-14 11:49:03 -0500

    done

--- app/core/bot/ab/matchers/ZeroMatcher.java
+++ app/core/bot/ab/matchers/ZeroMatcher.java
@@ -1,27 +1,26 @@
 /**
  *
  * Copyright 2015 RSVP Technologies Inc. All rights reserved.
- * SharpMatcher.java
+ * ZeroMatcher.java
  *
  */
 package core.bot.ab.matchers;
 
 import core.bot.ab.Nodemapper;
-import core.bot.ab.NodemapperOperator;
 import core.bot.ab.Path;
 
 /**
  *@author Shengying Pan (s5pan@uwaterloo.ca) 
  *@date Jan 14, 2015
  */
-public class SharpMatcher extends ZeroMatcher
+public class ZeroMatcher
 {
 
     @Override
     public Nodemapper match(Path path, Nodemapper node, String inputThatTopic, String starState, int starIndex, String[] inputStars,
-            String[] grammarStars, String[] thatStars, String[] topicStars, String matchTrace)
+            String[] grammarStars, String[] thatStars, String[] topicStars, String wildcard, String matchTrace)
     {
-        Nodemapper matchedNode;
+        matchTrace += "[" + wildcard + ",]";
         
     }
 
