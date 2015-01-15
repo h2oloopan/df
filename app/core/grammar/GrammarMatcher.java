ecc13ea734a79b1af55a7cc9d652feb7e95cb2ca
Shengying Pan <i@thepans.me>
2015-01-15 12:54:17 -0500

    some update

--- app/core/grammar/GrammarMatcher.java
+++ app/core/grammar/GrammarMatcher.java
@@ -5,5 +5,6 @@ import java.util.Map;
 public interface GrammarMatcher {
 	public GrammarMatcher initialize(String path) throws Exception;
 	public String match(String query) throws Exception;
+	public HashMap<String, String> getMap(String parsedQuery) throws Exception;
 }
 
