1f1f14f52037a20d3185a18bdfea7373b9ff25d3
Shengying Pan <i@thepans.me>
2015-01-20 09:57:21 -0500

    triple triple

--- app/core/context/Context.java
+++ app/core/context/Context.java
@@ -8,12 +8,15 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import play.libs.Json;
 import core.bot.Bot;
 import core.bot.ab.MagicStrings;
+import core.bot.ab.Triple;
 import core.messages.SpecialText;
 
 public class Context {
@@ -23,9 +26,21 @@ public class Context {
 	public ArrayList<String> queries;
 	public ArrayList<String> responses;
 	public HashMap<String, String> predicates;
+	public HashMap<String, String> vars;
+	
+	//TRIPLES
+	public HashMap<String, Triple> idTriple;
+	public HashMap<String, String> tripleStringId;
+	public HashMap<String, HashSet<String>> subjectTriples;
+	public HashMap<String, HashSet<String>> predicateTriples;
+	public HashMap<String, HashSet<String>> objectTriples;
+	private int idCnt = 0;
+	
 	
 	public Date dateCreated;
 	public Date dateExpired;
+	
+	
 	
 	public Context() {
 	    //Do not use this one
@@ -37,12 +52,130 @@ public class Context {
 		this.queries = new ArrayList<String>();
 		this.responses = new ArrayList<String>();
 		this.predicates = new HashMap<String, String>();
+		this.vars = new HashMap<String, String>();
 		this.dateCreated = new Date();
+		
+		idCnt = 0;
+		
+		//TRIPLES
+		this.idTriple = new HashMap<String, Triple>();
+		this.tripleStringId = new HashMap<String, String>();
+		this.subjectTriples = new HashMap<String, HashSet<String>>();
+		this.predicateTriples = new HashMap<String, HashSet<String>>();
+		this.objectTriples = new HashMap<String, HashSet<String>>();
 	}
 	
 	public void initialize(Bot bot) throws Exception {
 	    String predicatesPath = new File(new File(bot.config_path), "predicates.txt").getCanonicalPath();
 	    addPredicates(predicatesPath);
+	}
+	
+	//TRIPLE related functions
+	public String mapTriple(Triple triple) {
+	    String id = triple.id;
+        idTriple.put(id, triple);
+        String s, p, o;
+        s = triple.subject;
+        p = triple.predicate;
+        o = triple.object;
+
+        s = s.toUpperCase();
+        p = p.toUpperCase();
+        o = o.toUpperCase();
+
+        String tripleString = s+":"+p+":"+o;
+        tripleString = tripleString.toUpperCase();
+
+        if (tripleStringId.keySet().contains(tripleString)) {
+            return tripleStringId.get(tripleString); // triple already exists
+        }
+        else {
+            tripleStringId.put(tripleString, id);
+
+            HashSet<String> existingTriples;
+            if (subjectTriples.containsKey(s)) existingTriples = subjectTriples.get(s);
+            else existingTriples = new HashSet<String>();
+            existingTriples.add(id);
+            subjectTriples.put(s, existingTriples);
+
+            if (predicateTriples.containsKey(p)) existingTriples = predicateTriples.get(p);
+            else existingTriples = new HashSet<String>();
+            existingTriples.add(id);
+            predicateTriples.put(p, existingTriples);
+
+            if (objectTriples.containsKey(o)) existingTriples = objectTriples.get(o);
+            else existingTriples = new HashSet<String>();
+            existingTriples.add(id);
+            objectTriples.put(o, existingTriples);
+
+            return id;
+        }
+	}
+	
+	public String unMapTriple(Triple triple) {
+	    String id = MagicStrings.undefined_triple;
+        String s, p, o;
+        s = triple.subject;
+        p = triple.predicate;
+        o = triple.object;
+
+        s = s.toUpperCase();
+        p = p.toUpperCase();
+        o = o.toUpperCase();
+
+        String tripleString = s+":"+p+":"+o;
+
+        System.out.println("unMapTriple "+tripleString);
+        tripleString = tripleString.toUpperCase();
+
+
+        triple = idTriple.get(tripleStringId.get(tripleString));
+
+        System.out.println("unMapTriple "+triple);
+        if (triple != null) {
+        id = triple.id;
+        idTriple.remove(id);
+        tripleStringId.remove(tripleString);
+
+        HashSet<String> existingTriples;
+        if (subjectTriples.containsKey(s)) existingTriples = subjectTriples.get(s);
+        else existingTriples = new HashSet<String>();
+        existingTriples.remove(id);
+        subjectTriples.put(s, existingTriples);
+
+        if (predicateTriples.containsKey(p)) existingTriples = predicateTriples.get(p);
+        else existingTriples = new HashSet<String>();
+        existingTriples.remove(id);
+        predicateTriples.put(p, existingTriples);
+
+        if (objectTriples.containsKey(o)) existingTriples = objectTriples.get(o);
+        else existingTriples = new HashSet<String>();
+        existingTriples.remove(id);
+        objectTriples.put(o, existingTriples);
+        }
+        else id = MagicStrings.undefined_triple;
+
+        return id;
+	}
+	
+	public Set<String> allTriples() {
+	    return new HashSet<String>(idTriple.keySet());
+	}
+	
+	public String addTriple(String subject, String predicate, String object) {
+	    if (subject == null || predicate == null || object == null) return MagicStrings.undefined_triple;
+	    String tid = uid + "_" + sid + idCnt++;
+	    Triple triple = new Triple(tid, subject, predicate, object);
+	    String id = mapTriple(triple);
+	    return id;
+	}
+	
+	public String deleteTriple(String subject, String predicate, String object) {
+	    if (subject == null || predicate == null || object == null) return MagicStrings.undefined_triple;
+	    String tid = uid + "_" + sid + idCnt++;
+        Triple triple = new Triple(tid, subject, predicate, object);
+        String id = unMapTriple(triple);
+        return id;
 	}
 	
 	//PREDICATES related functions
