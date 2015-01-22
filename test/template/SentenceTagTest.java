0e8d1a1d73e57fc5f71304f485375c695ace92ec
Shengying Pan <i@thepans.me>
2015-01-22 14:33:59 -0500

    unit test still working

--- test/template/SentenceTagTest.java
+++ test/template/SentenceTagTest.java
@@ -1,12 +1,14 @@
 /**
  *
  * Copyright 2015 RSVP Technologies Inc. All rights reserved.
- * FormalTagTest.java
+ * SentenceTagTest.java
  *
  */
 package template;
 
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.w3c.dom.Node;
 
 import supply.dummies.BotFactory;
@@ -20,7 +22,7 @@ import core.context.Context;
  *@author Shengying Pan (s5pan@uwaterloo.ca) 
  *@date Jan 22, 2015
  */
-public class FormalTagTest
+public class SentenceTagTest
 {
     private TagHandlerCollection handlers;
     
@@ -30,30 +32,30 @@ public class FormalTagTest
     }
     
     @Test
-    public void formalSimple() {
+    public void sentenceSimple() {
         try {
-            System.out.println("Testing formalizing simple text");
-            String template = "<template>What, <formal>hello kitty</formal></template>";
+            System.out.println("Testing sentencing simple text");
+            String template = "<template>What, <sentence>hello kitty</sentence></template>";
             Node node = DomUtils.parseString(template);
             ParseState ps = new ParseState();
             ps.bot = BotFactory.getDummyBot();
             TagHandler defaultHandler = handlers.getDefaultHandler();
             String result = defaultHandler.handle(node, ps);
-            Assert.assertEquals("What, Hello Kitty", result);
+            Assert.assertEquals("What, Hello kitty", result);
         } catch (Exception e) {
             Assert.fail("Should not get exception here, but " + e.getMessage());
         }
     }
     
     @Test
-    public void formalComplex() {
+    public void sentenceComplex() {
         try {
-            System.out.println("Testing formalizing complex text");
-            String template = "<template>What, <formal>"
-                    + "Hello, nice to meet you "
+            System.out.println("Testing sentencing complex text");
+            String template = "<template>What, <sentence>"
+                    + "hello, nice to meet you "
                     + "<condition name=\"gender\" value=\"female\">美女</condition>"
                     + "<condition name=\"gender\" value=\"male\">帅哥</condition>"
-                    + "</formal></template>";
+                    + "</sentence></template>";
             Node node = DomUtils.parseString(template);
             ParseState ps = new ParseState();
             ps.bot = BotFactory.getDummyBot();
@@ -62,7 +64,7 @@ public class FormalTagTest
             ps.context = context;
             TagHandler defaultHandler = handlers.getDefaultHandler();
             String result = defaultHandler.handle(node, ps);
-            Assert.assertEquals("What, Hello, Nice To Meet You 帅哥", result);
+            Assert.assertEquals("What, Hello, nice to meet you 帅哥", result);
         } catch (Exception e) {
             Assert.fail("Should not get exception here, but " + e.getMessage());
         }
