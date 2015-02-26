2ed0875fd6856ee01ab33d01621966f96acb34ea
Shengying Pan <i@thepans.me>
2015-02-26 10:10:00 -0500

    work on

---
+++ app/platform/models/User.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * Copyright 2015 RSVP Technologies Inc. All rights reserved.
+ * User.java
+ *
+ */
+package platform.models;
+
+/**
+ *@author Shengying Pan (s5pan@uwaterloo.ca) 
+ *@date Feb 26, 2015
+ */
+public class User
+{
+
+}
